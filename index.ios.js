334054beccd02ef53cea60317fb15f08c2253a19
Steve Dakh <slavik0329@gmail.com>
2016-01-23 11:03:41 -0500

    Start screen and Home screen

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-class reactSkeleton extends Component {
-  render() {
+var React = require('react-native');
+var Main = require('./main.js');
+
+import { Provider } from 'react-redux/native'
+import configureStore from './app/store/configure-store'
+
+const store = configureStore()
+
+var {
+  AppRegistry,
+  StyleSheet,
+  StatusBarIOS,
+  View,
+} = React;
+
+var reactSkeleton = React.createClass({
+  componentDidMount() {
+      StatusBarIOS.setStyle("light-content");  
+  },
+
+  render: function() {
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Provider store={store}>
+        {() => <Main/>}
+      </Provider>
     );
   }
-}
+});
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('reactSkeleton', () => reactSkeleton);
